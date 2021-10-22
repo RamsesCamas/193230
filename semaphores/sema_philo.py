5555e09f0c9c4c613d012ca6485c7eaaa22104e2
RamsesCamas <machiniram@gmail.com>
2021-10-22 01:15:23 -0500

    Tries of solution

--- semaphores/sema_philo.py
+++ semaphores/sema_philo.py
@@ -1,41 +1,46 @@
 import threading
 import time
 
+
 class TenedorFilosofo(threading.Thread):
     def __init__(self, tenedores, filosofosNum):
         threading.Thread.__init__(self)
         self.tenedores = tenedores
         self.filosofosNum = filosofosNum
-        self.datoTemporal =  self.filosofosNum + 1 % 5
+        self.datoTemporal =  (filosofosNum + 1) % 5
+        self.start()
    
     def hilosFilosofos(self):
         while True:
             print("Filosofo iniciando", self.filosofosNum)
             time.sleep(2)
-            print("Filosofo ", self.filosofosNum, "pasando tenedor del lado izquierdo")
-            self.tenedores[self.filosofosNum].acquire()
-            time.sleep(2)
-            print("Filosofo ", self.filosofosNum, "recoge tenedor del lado derecho")
             self.tenedores[self.datoTemporal].acquire()
-            time.sleep(2)
-            print("Filosofo ", self.filosofosNum, "libre derecho")
+            print("Filosofo ", self.filosofosNum, "recoge tenedor del lado derecho")
+            time.sleep(1)
+
+            print("Filosofo ", self.filosofosNum, "recoge tenedor del lado izquierdo")
+            self.tenedores[self.filosofosNum].acquire()
+            time.sleep(1)            
+            
+            print("Filosofo ", self.filosofosNum, "libre izquierdo")
             self.tenedores[self.datoTemporal].release()
             time.sleep(2)
-            print("Filosofo ", self.filosofosNum, "libre izquierdo")
+
             self.tenedores[self.filosofosNum].release()
+            print("Filosofo ", self.filosofosNum, "libre derecho")
             time.sleep(2)
    
     def run(self):
         self.hilosFilosofos()
+        
 
 
 tenedorArray = [1,1,1,1,1]
 
-for i in range(0,5):
-    print("for uno: ", i)
-    tenedorArray[i] = threading.BoundedSemaphore(1)
+if __name__ == '__main__':
+    for i in range(0,5):
+        tenedorArray[i] = threading.BoundedSemaphore(2)
 
-for i in range(0,5):
-    print("for dos: ", i)
-    total = TenedorFilosofo(tenedorArray, i)
-    total.start()
+    for i in range(0,5):
+        total = TenedorFilosofo(tenedorArray, i)
+        time.sleep(2)
